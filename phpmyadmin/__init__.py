cd74f4863b2999171a5df2a44ef1f541cf0cd991
indrajit <eendroroy@gmail.com>
2017-05-06 22:43:26 +0600

    pull image if not exists

--- phpmyadmin/__init__.py
+++ phpmyadmin/__init__.py
@@ -1,9 +1,14 @@
-from phpmyadmin import invoke
+from phpmyadmin import phpmyadmin_main
+from termcolor import colored
 
 VERSION = '0.0.0'
 
 
 def main():
     """Entry point for the application script"""
-    print("ITS A DUMMY YET.\nWait Version >= 0.0.1")
-    invoke()
+    print
+    print(colored("VERSION: %s" % VERSION, 'blue'))
+    print(colored("ITS A DUMMY YET", 'blue'))
+    print(colored("Wait for Version >= 0.0.1", 'blue'))
+    print
+    phpmyadmin_main()
--- phpmyadmin/phpmyadmin.py
+++ phpmyadmin/phpmyadmin.py
@@ -1,11 +1,25 @@
 import sys
 
-from support import get_config
+from support import check_docker, get_config, get_docker_version, check_image, pull_image
+from termcolor import colored
+
+IMAGE_NAME = 'phpmyadmin/phpmyadmin'
 
 
-def invoke():
+def get_valid_config():
     config = get_config()
     if config is None:
         sys.exit(1)
-    else:
-        print(config)
+    if not check_docker():
+        sys.exit(1)
+
+    return config
+
+
+def phpmyadmin_main():
+    config = get_valid_config()
+
+    if not check_image(IMAGE_NAME, config.get('image_version')):
+        print(pull_image(IMAGE_NAME, config.get('image_version')))
+
+    print(colored("image ok :)", 'green'))
